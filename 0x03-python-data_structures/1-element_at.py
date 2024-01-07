0e14407a3a3a6f160746109a2290f85edc154e58
ben obame <obameben@yahoo.fr>
2024-01-07 17:27:19 +0200

    0x03-1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,14 +1,16 @@
 #!/usr/python
 
 def element_at(my_list, idx):
-    """Retrieves an element from a list, handling out-of-range and negative indices.
+    """Retrieves an element from a list, handling out-of-range
+    and negative indices.
 
     Args:
         my_list: The list to access.
         idx: The index of the element to retrieve.
 
     Returns:
-        The element at the specified index, or None if the index is out of range or negative.
+        The element at the specified index, or
+        None if the index is out of range or negative.
     """
 
     if idx < 0 or idx >= len(my_list):
