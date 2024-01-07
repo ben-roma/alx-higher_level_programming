3eda873dd2f6f28df543e198e97223234a36cdf9
ben obame <obameben@yahoo.fr>
2024-01-07 17:54:42 +0200

    0x03-2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -15,7 +15,7 @@ def replace_in_list(my_list, idx, element):
     """
 
     if idx < 0 or idx >= len(my_list):
-        return my_list  # Return original list for invalid indices
+        return my_list
     else:
-        my_list[idx] = element  # Replace the element at the specified index
-        return my_list  # Return the modified list
+        my_list[idx] = element
+        return my_list
