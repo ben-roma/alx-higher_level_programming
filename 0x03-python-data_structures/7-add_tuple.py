17a724423241805d847e1434491169a864511021
ben obame <obameben@yahoo.fr>
2024-01-08 01:27:31 +0200

    0x03-7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Adds two tuples element-wise, handling different lengths
+    and returning a tuple with the first two sums.
+
+    Args:
+        tuple_a: The first tuple.
+        tuple_b: The second tuple.
+
+    Returns:
+        A tuple containing the sum of the first two elements of each tuple.
+    """
+
+    first_elements = (tuple_a + (0, 0))[:2] 
+    second_elements = (tuple_b + (0, 0))[:2]
+    return (first_elements[0] + second_elements[0],
+            first_elements[1] + second_elements[1])
