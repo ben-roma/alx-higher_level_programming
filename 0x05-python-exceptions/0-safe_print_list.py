f2fdef4579962979c09a5916da8e15d2b10849ed
ben obame <obameben@yahoo.fr>
2024-01-25 11:50:09 +0200

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,9 +14,9 @@ def safe_print_list(my_list=[], x=0):
     printed_count = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")  # Print elements without newline
+            print(my_list[i], end="")
             printed_count += 1
     except IndexError:
-        pass  # Silently handle IndexError if x exceeds list length
-    print()  # Add a newline after printing elements
+        pass
+    print() 
     return printed_count
