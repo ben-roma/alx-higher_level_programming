cbcbadd0afa215fe0511612fd83580c612ebc6cc
ben obame <obameben@yahoo.fr>
2024-02-01 00:54:30 +0200

    task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defining a class Square in program"""
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initializes the square with the given size."""
+        self.__size = size
