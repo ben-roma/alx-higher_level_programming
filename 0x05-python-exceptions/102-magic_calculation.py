30c28a4012dc03fa2d4b00dfb9db923629669ed0
ben obame <obameben@yahoo.fr>
2024-02-27 16:15:05 +0200

    task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,12 +3,11 @@
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 4):
-        try:
-            if i > a:
-                raise Exception("Too far")
-            else:
-                result += a ** b / i
-        except Exception:
-            result = a + b
+        if i > a:
             break
+        try:
+            result += a ** b / i
+        except Exception as err:
+            print("Exception:", err, file=sys.stderr)
+            return None
     return result
