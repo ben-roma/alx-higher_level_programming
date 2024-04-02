726da8c36e7f414c97f9d96286a865d05731dcea
ben obame <obameben@yahoo.fr>
2024-04-02 12:48:31 +0200

    task  0

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list.
+Defines a class MyList that inherits from list.
+"""
+
+
+class MyList(list):
+    """
+    A subclass of list with a method to print the list in sorted order.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending order without modifying the original list.
+        """
+        print(sorted(self))
