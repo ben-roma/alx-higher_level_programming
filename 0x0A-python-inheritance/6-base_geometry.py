d93e219af0a59cdf67d8a7c8cd65b101638d28f1
ben obame <obameben@yahoo.fr>
2024-04-03 16:02:07 +0200

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry.
+Defines a class BaseGeometry with a public instance method area.
+"""
+
+
+class BaseGeometry:
+    """A class with a method area not yet implemented."""
+
+    def area(self):
+        """Raises an Exception with a message saying
+        the method is not implemented."""
+        raise Exception("area() is not implemented")
