063cca357cecbc7bb2e818c8400affcf78132557
ben obame <obameben@yahoo.fr>
2024-04-08 20:23:37 +0200

    task 9 & 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(argv[2], 10);
+console.log(factorial(num));
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+function add(a, b) {
+  return a + b;
+}
+
+const num1 = parseInt(argv[2], 10);
+const num2 = parseInt(argv[3], 10);
+
+console.log(add(num1, num2));
