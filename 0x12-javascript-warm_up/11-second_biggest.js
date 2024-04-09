1c2d12fd0b1838354887ab0282b9748423d4d107
ben obame <obameben@yahoo.fr>
2024-04-09 19:16:29 +0200

    task 9 & 10

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 const { argv } = require('process');
 
 if (argv.length <= 3) {
-	console.log(0);
+  console.log(0);
 } else {
-	const numbers = argv.slice(2).map(Number);
-	const sortedNumbers = numbers.sort((a, b) => a - b).reverse();
-	console.log(sortedNumbers[1]);
+  const numbers = argv.slice(2).map(Number);
+  const sortedNumbers = numbers.sort((a, b) => a - b).reverse();
+  console.log(sortedNumbers[1]);
 }
