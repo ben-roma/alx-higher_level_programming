c7a6171cc0f06cbc0e15aac983da76b4116345e0
ben obame <obameben@yahoo.fr>
2024-04-09 19:46:41 +0200

    update

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
-  value: 12
+  value: 12,
+  incr: function () {
+    this.value += 1;
+  }
 };
+
 console.log(myObject);
-myObject.incr = () => { myObject.value += 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
