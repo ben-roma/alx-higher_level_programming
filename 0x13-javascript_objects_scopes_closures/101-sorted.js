5c06ac21b55f20f96ee1031fc41f52aa524a9c6f
ben obame <obameben@yahoo.fr>
2024-04-09 21:05:21 +0200

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const { dict } = require('./101-data');
+const { dict } = require('./101-data.js').dict;
 console.log(dict);
 
 const newDict = Object.entries(dict).reduce((acc, [userId, occurrence]) => {
