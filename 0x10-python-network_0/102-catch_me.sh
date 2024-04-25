45780611567e5ab27b8363ed3bc5183c6f16c004
ben obame <obameben@yahoo.fr>
2024-04-26 00:02:50 +0200

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to make a request that causes the server to respond with "You got me!"
-curl -sLX PUT "0.0.0.0:5000/catch_me" -H "Origin:You got me!"
+curl -sLX PUT "0.0.0.0:5000/catch_me" -H "Origin: HolbertonSchool"
